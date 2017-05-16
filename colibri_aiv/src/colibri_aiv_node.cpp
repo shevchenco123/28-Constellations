f75a224924cc095533bd2729a63ba9c4d2354632
shevchenco123 <1007380426@qq.com>
2017-05-16 11:46:38 +0800

    huge modify in colibri_aiv for baseline ver

--- colibri_aiv/src/colibri_aiv_node.cpp
+++ colibri_aiv/src/colibri_aiv_node.cpp
@@ -1,19 +1,20 @@
-#include "colibri_aiv.h" 
+#include "aiv_driver.h" 
 
 int main(int argc, char* argv[])
 {	
-	ros::init(argc, argv, "twist_odom_node");
-	colibri_serial my_port;
-	my_port.init_port("/dev/ttyUSB0");
-	my_port.creat_thread(read_data_thread);
-	//my_port.send_cmd(my_port.enable_motor,colibri_serial::enable_motor_finish);
-	//my_port.send_cmd(my_port.req_velocity_start,colibri_serial::req_velocity_start_finish);
+	ros::init(argc, argv, "aiv_driver_node");
+	AIV_Driver my_port;
+	my_port.InitCom("/dev/ttyUSB0");
+	my_port.CreateThread(ReadDataThread);
+	
+	//my_port.SendCmd(my_port.enable_motor,AIV_Driver::enable_motor_finish);
+	//my_port.SendCmd(my_port.req_vel_start,AIV_Driver::req_vel_start_finish);
 
-	//my_port.send_cmd(my_port.req_encoder_start,colibri_serial::req_encoder_start_finish);
+	//my_port.SendCmd(my_port.req_encoder_start,AIV_Driver::req_encoder_start_finish);
 
-	//my_port.send_cmd(my_port.req_ultrasonic_start,colibri_serial::req_ultrasonic_start_finish);
+	//my_port.SendCmd(my_port.req_ultra_start,AIV_Driver::req_ultra_start_finish);
 
-	//my_port.send_cmd(my_port.req_collision_start,colibri_serial::req_collision_start_finish);
+	//my_port.SendCmd(my_port.req_bumper_start,AIV_Driver::req_bumper_start_finish);
 
 	ros::spin();
 
