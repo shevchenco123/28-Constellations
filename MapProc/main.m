1657a0489ba1c179a566ea18d9e3799275945f49
shevchenco123 <1007380426@qq.com>
2017-05-18 17:34:27 +0800

    add comments in map_process

--- MapProc/main.m
+++ MapProc/main.m
@@ -12,7 +12,8 @@ map_sz = size(nav_map);
 len = 50;
 h_x = map_sz(1,2)/2.0 -len/2;
 h_y = map_sz(1,1)/2.0 -len/2;
-h = imrect(gca, [h_x h_y len len]);  
+
+h = imrect(gca, [h_x h_y len len]);  %load ROI area
 api = iptgetapi(h);  
 api.addNewPositionCallback(@(p) title(mat2str(p,3)));  
 fcn = makeConstrainToRectFcn('imrect',get(gca,'XLim'),get(gca,'YLim'));  
@@ -20,7 +21,7 @@ api.setPositionConstraintFcn(fcn);
 
 hold on;
 
-map_proc = 1;
+map_proc = 1;   % branch proc flags for set init/target point,nav path poing,fill/clear/unknown a area
 exit_flag = 0;
 init_flag = 0;
 target_flag = 0;
@@ -29,17 +30,19 @@ fill_flag = 0;
 clear_flag = 0;
 unknown_flag = 0;
 
-black_gray = 0;
+black_gray = 0;     % gray value for modify the ROI and should not changed for ROS systerm
 white_gray = 254;
 unknown_gray = 205;
 
 while(map_proc)
    op = input('Input map operation mode: ');
    switch op
+       %% input 0 exit the map process 
        case 0
            disp('--- Exit Map Process Mode...');
            exit_flag = 1;
            break;
+       %% 
        case 1
            disp('+++ Set Robot Target Pos...');
            if(target_flag == 0)
@@ -48,52 +51,58 @@ while(map_proc)
                 target_flag = 1;
            end
            hold on;
+       %%
        case 2
            disp('+++ Set Robot Init Pos...');
            if(init_flag == 0)
                 [rec, center] = CalcRecParam(h);
-                plot(center(1, 1), center(1, 2),'s', 'MarkerSize', 8, 'MarkerEdgeColor', 'g', 'MarkerFaceColor', 'g');
+                plot(center(1, 1), center(1, 2),'s', 'MarkerSize', 6, 'MarkerEdgeColor', 'g', 'MarkerFaceColor', 'g');
                 init_flag = 1;
            end
            hold on;
+       %%
        case 3
            disp('+++ Fill an Rec Area...');
            if(fill_flag == 0)
                 [rec, center] = CalcRecParam(h);
                 nav_map = ModifyGreyValue( nav_map, black_gray, rec);
-                rectangle('Position',rec,'LineWidth',1,'FaceColor',[0.004 0.004 0.004],'EdgeColor','k');
-                plot(center(1, 1), center(1, 2),'+', 'MarkerSize', 8, 'MarkerEdgeColor', 'g', 'MarkerFaceColor', 'y');
+                rectangle('Position',rec,'LineWidth',0.1,'FaceColor',[0.004 0.004 0.004],'EdgeColor','y');
+                plot(center(1, 1), center(1, 2),'+', 'MarkerSize', 4, 'MarkerEdgeColor', 'g', 'MarkerFaceColor', 'y');
                 fill_flag = 1;
            end
            hold on;
+       %%
        case 4
            disp('+++ Clear an Rec Area...');
            if(clear_flag == 0)
                 [rec, center] = CalcRecParam(h);
                 nav_map = ModifyGreyValue( nav_map, white_gray, rec);
-                rectangle('Position',rec,'LineWidth',1,'FaceColor',[0.996 0.996 0.996],'EdgeColor','k');
-                plot(center(1, 1), center(1, 2),'o', 'MarkerSize', 8, 'MarkerEdgeColor', 'g', 'MarkerFaceColor', 'y');
+                rectangle('Position',rec,'LineWidth',0.1,'FaceColor',[0.996 0.996 0.996],'EdgeColor','y');
+                plot(center(1, 1), center(1, 2),'.', 'MarkerSize', 6, 'MarkerEdgeColor', 'g', 'MarkerFaceColor', 'y');
                 clear_flag = 1;
            end
            hold on;
+       %%
        case 5
            disp('+++ Unknow an Rec Area...');
            if(unknown_flag == 0)
                 [rec, center] = CalcRecParam(h);
                 nav_map = ModifyGreyValue( nav_map, unknown_gray, rec);
-                rectangle('Position',rec,'LineWidth',1,'FaceColor',[0.804 0.804 0.804],'EdgeColor','k');
-                plot(center(1, 1), center(1, 2),'*', 'MarkerSize', 8, 'MarkerEdgeColor', 'g', 'MarkerFaceColor', 'y');
+                rectangle('Position',rec,'LineWidth',0.1,'FaceColor',[0.804 0.804 0.804],'EdgeColor','y');
+                plot(center(1, 1), center(1, 2),'*', 'MarkerSize', 6, 'MarkerEdgeColor', 'g', 'MarkerFaceColor', 'y');
                 unknown_flag = 1;
            end
            hold on;
+       %%
        case 6
            disp('+++ Set Robot Nav Path Point...');
            if(nav_flag == 0)
                 [rec, center] = CalcRecParam(h);
-                plot(center(1, 1), center(1, 2),'x', 'MarkerSize', 8, 'MarkerEdgeColor', 'y');
+                plot(center(1, 1), center(1, 2),'x', 'MarkerSize', 6, 'MarkerEdgeColor', 'b');
                 nav_flag = 1;
            end
            hold on;
+       %%
        case 9
            disp('...Reset flags for New Operation...');
            init_flag = 0;
@@ -111,6 +120,7 @@ while(map_proc)
        break;
    end
 end
+
 figure(2)
 imshow(nav_map);
 saveas(gcf, 'correct', 'pgm');
