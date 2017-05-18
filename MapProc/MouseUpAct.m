09743d3eb4adaa67a646a3a6c131e248527cb82e
shevchenco123 <1007380426@qq.com>
2017-05-18 15:32:56 +0800

    modify ros map .pgm file

--- MapProc/MouseUpAct.m
+++
@@ -1,9 +0,0 @@
-function [ ] = MouseUpAct( src,event )
-%MOUSECLICKFCN Summary of this function goes here
-%   Detailed explanation goes here
-    global mouse_down_flag;
-    if(mouse_down_flag == 1)   
-      mouse_down_flag = 0;
-    end
-end
-
--- MapProc/main.m
+++ MapProc/main.m
@@ -1,51 +1,116 @@
 clc;
 clear;
 close all;
-map_proc = 1;
-exit_flag = 0;
-
-global nav_path;
-global point_cnt;
-nav_path = zeros(2,10);
-point_cnt = 0;
 
 map_name = 'map_set/fac505.pgm';
 nav_map = imread(map_name);
-disp('Load Map Picture...')
+disp('Load Map Picture...');
+
+figure(1);
 imshow(nav_map);
+map_sz = size(nav_map);
+len = 50;
+h_x = map_sz(1,2)/2.0 -len/2;
+h_y = map_sz(1,1)/2.0 -len/2;
+h = imrect(gca, [h_x h_y len len]);  
+api = iptgetapi(h);  
+api.addNewPositionCallback(@(p) title(mat2str(p,3)));  
+fcn = makeConstrainToRectFcn('imrect',get(gca,'XLim'),get(gca,'YLim'));  
+api.setPositionConstraintFcn(fcn);  
+
 hold on;
 
-global mouse_down_flag
-mouse_down_flag = 1;
-global target
-target = zeros(2,1);
+map_proc = 1;
+exit_flag = 0;
+init_flag = 0;
+target_flag = 0;
+nav_flag = 0;
+fill_flag = 0;
+clear_flag = 0;
+unknown_flag = 0;
 
+black_gray = 0;
+white_gray = 254;
+unknown_gray = 205;
 
 while(map_proc)
    op = input('Input map operation mode: ');
    switch op
        case 0
-           disp('Exit Map Process Mode...');
+           disp('--- Exit Map Process Mode...');
            exit_flag = 1;
            break;
        case 1
-           disp('Set Robot Target Pos...');
-           set(gcf,'WindowButtonDownFcn',@MouseDownAct);
-           set(gcf,'WindowButtonUpFcn',@MouseUpAct);
+           disp('+++ Set Robot Target Pos...');
+           if(target_flag == 0)
+                [rec, center] = CalcRecParam(h);
+                plot(center(1, 1), center(1, 2),'p', 'MarkerSize', 8, 'MarkerEdgeColor', 'r', 'MarkerFaceColor', 'r');
+                target_flag = 1;
+           end
            hold on;
        case 2
-           disp('Selet a Rectangle Area...');
-           set(gcf,'WindowButtonDownFcn',@MouseSetRectange);
-           hold on;         
+           disp('+++ Set Robot Init Pos...');
+           if(init_flag == 0)
+                [rec, center] = CalcRecParam(h);
+                plot(center(1, 1), center(1, 2),'s', 'MarkerSize', 8, 'MarkerEdgeColor', 'g', 'MarkerFaceColor', 'g');
+                init_flag = 1;
+           end
+           hold on;
+       case 3
+           disp('+++ Fill an Rec Area...');
+           if(fill_flag == 0)
+                [rec, center] = CalcRecParam(h);
+                nav_map = ModifyGreyValue( nav_map, black_gray, rec);
+                rectangle('Position',rec,'LineWidth',1,'FaceColor',[0.004 0.004 0.004],'EdgeColor','k');
+                plot(center(1, 1), center(1, 2),'+', 'MarkerSize', 8, 'MarkerEdgeColor', 'g', 'MarkerFaceColor', 'y');
+                fill_flag = 1;
+           end
+           hold on;
+       case 4
+           disp('+++ Clear an Rec Area...');
+           if(clear_flag == 0)
+                [rec, center] = CalcRecParam(h);
+                nav_map = ModifyGreyValue( nav_map, white_gray, rec);
+                rectangle('Position',rec,'LineWidth',1,'FaceColor',[0.996 0.996 0.996],'EdgeColor','k');
+                plot(center(1, 1), center(1, 2),'o', 'MarkerSize', 8, 'MarkerEdgeColor', 'g', 'MarkerFaceColor', 'y');
+                clear_flag = 1;
+           end
+           hold on;
+       case 5
+           disp('+++ Unknow an Rec Area...');
+           if(unknown_flag == 0)
+                [rec, center] = CalcRecParam(h);
+                nav_map = ModifyGreyValue( nav_map, unknown_gray, rec);
+                rectangle('Position',rec,'LineWidth',1,'FaceColor',[0.804 0.804 0.804],'EdgeColor','k');
+                plot(center(1, 1), center(1, 2),'*', 'MarkerSize', 8, 'MarkerEdgeColor', 'g', 'MarkerFaceColor', 'y');
+                unknown_flag = 1;
+           end
+           hold on;
+       case 6
+           disp('+++ Set Robot Nav Path Point...');
+           if(nav_flag == 0)
+                [rec, center] = CalcRecParam(h);
+                plot(center(1, 1), center(1, 2),'x', 'MarkerSize', 8, 'MarkerEdgeColor', 'y');
+                nav_flag = 1;
+           end
+           hold on;
        case 9
-           disp('Waiting for New Operation...');
-           mouse_down_flag = 1;
+           disp('...Reset flags for New Operation...');
+           init_flag = 0;
+           target_flag = 0;
+           nav_flag = 0;
+           fill_flag = 0;
+           clear_flag = 0;
+           unknown_flag = 0;
+           
        otherwise
-           disp('An Error Input Number !!!');
+           disp('~~~ An Error Input Mode Param !!!');
    end
    
    if(exit_flag == 1)
        break;
    end
 end
-
+figure(2)
+imshow(nav_map);
+saveas(gcf, 'correct', 'pgm');
