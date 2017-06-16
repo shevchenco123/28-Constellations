19751777d2bbac1d23915414d208d57ea27c4f81
shevchenco123 <1007380426@qq.com>
2017-06-16 11:10:10 +0800

    modify aux pub and add exec in CMakeList.txt

--- colibri_avoidence/src/colibri_aux.cpp
+++ colibri_avoidence/src/colibri_aux.cpp
@@ -2,14 +2,15 @@
 #include "geometry_msgs/Twist.h"
 #include "colibri_msgs/EnvSecurity.h"
 #include "colibri_msgs/AuxInfo.h"
-#include "cartodom/Carotodom.h"
+#include "cartodom/Cartodom.h"
+
 
 geometry_msgs::Twist aux_twist;
-colibri_msgs::EnvSecure aux_envsec;
+colibri_msgs::EnvSecurity aux_envsec;
 cartodom::Cartodom aux_cartodom;
 
 void SubTwistCallBack(const geometry_msgs::Twist::ConstPtr & cmd_vel);
-void SubEnvSecurityCallBack(const colibri_msgs::EnvSecure::ConstPtr & env_sec);
+void SubEnvSecurityCallBack(const colibri_msgs::EnvSecurity::ConstPtr & env_sec);
 void StateFbCallback(const cartodom::Cartodom::ConstPtr & carto_odom);
 
 
@@ -17,21 +18,23 @@ int main(int argc, char* argv[])
 {	
 
 	// ROS nav node initial
-	ros::init(argc, argv, "Auxiliary Info Pub");
+	ros::init(argc, argv, "Auxiliary_Info_Pub");
 	ROS_INFO("Start to Pub the Auxinfo ... ");
 	
 	ros::NodeHandle nh_aux;
 	ros::Subscriber twist_sub;
 	ros::Subscriber env_sub;
 	ros::Subscriber state_fb_sub;
-	ros::Publisher aux_info_pub;
+	ros::Publisher aux_pub;
 
 	ros::Rate loop_rate(10);	
 	
 	colibri_msgs::AuxInfo aux_info;
-	twist_sub = nh_aux.subscribe<geometry_msgs::Twist>("/t_cmd_vel", 1, &SubTwistCallBack, this);
-	env_sub = nh_aux.subscribe<colibri_msgs::EnvSecure>("/env_secure", 1, &SubEnvSecurityCallBack, this);
-	state_fb_sub = nh_aux.subscribe<cartodom::Cartodom>("/cartodom", 1, &StateFbCallback, this);
+	twist_sub = nh_aux.subscribe<geometry_msgs::Twist>("/t_cmd_vel", 1, &SubTwistCallBack);
+	env_sub = nh_aux.subscribe<colibri_msgs::EnvSecurity>("/env_secure", 1, &SubEnvSecurityCallBack);
+	state_fb_sub = nh_aux.subscribe<cartodom::Cartodom>("/cartodom", 1, &StateFbCallback);
+
+	aux_pub = nh_aux.advertise<colibri_msgs::AuxInfo>("/aux_info", 1);
 
 	while(ros::ok())
 	{
@@ -48,7 +51,7 @@ int main(int argc, char* argv[])
 		aux_info.ultra_switch = 0;
 		aux_info.ros_fault = 0;
 
-		aux_info_pub.publish(aux_info);
+		aux_pub.publish(aux_info);
 		
 		ros::spinOnce();
 		loop_rate.sleep();
@@ -65,7 +68,7 @@ void SubTwistCallBack(const geometry_msgs::Twist::ConstPtr & cmd_vel)
 	aux_twist.angular.z = cmd_vel->angular.z;
 }
 
-void SubEnvSecurityCallBack(const colibri_msgs::EnvSecure::ConstPtr & env_sec)
+void SubEnvSecurityCallBack(const colibri_msgs::EnvSecurity::ConstPtr & env_sec)
 {
 	aux_envsec.laser_min_dis = env_sec->laser_min_dis;
 	aux_envsec.laser_min_angle = env_sec->laser_min_angle;
