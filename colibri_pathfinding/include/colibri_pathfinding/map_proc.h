542f4940518920b57998d2c7866b170535d1145d
shevchenco123 <1007380426@qq.com>
2017-07-09 11:50:44 +0800

    add two map_proc module file

---
+++ colibri_pathfinding/include/colibri_pathfinding/map_proc.h
@@ -0,0 +1,30 @@
+#include ""
+
+
+#ifndef _MAP_PROC_H_
+#define _MAP_PROC_H_
+
+
+
+
+
+
+// subscribe the goal info
+
+// pub the topic path
+
+// read the task yaml
+
+// map inflaion for path planning
+
+// point pix <  --- > phiscal pos  
+
+// path pix (smooth or not)  < ---> phisical path
+
+// goal edge adjustment
+
+//loacl  map data update for new plan
+ 
+
+
+#endif
