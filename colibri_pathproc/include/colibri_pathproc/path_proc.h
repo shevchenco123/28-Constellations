55150bb9e2f3a2d79b96942d5495c2665164d67e
shevchenco123 <1007380426@qq.com>
2017-08-04 16:50:32 +0800

    add the pathproc frame and the bresenhamLine func

--- colibri_pathproc/include/colibri_pathproc/path_proc.h
+++ colibri_pathproc/include/colibri_pathproc/path_proc.h
@@ -1,31 +1,58 @@
 #ifndef _PATH_PROC_H_
 #define _PATH_PROC_H_
 
+#include <fstream>
+#include "yaml-cpp/yaml.h"
+#include <math.h>
+#include <vector>
+
+template <class T>
+typedef struct st_point2D{
+	T x;
+	T y;
+}point2d;
 
 
+template <class T>
+typedef struct st_segment{
+	int seg_id;
+	int start_id;
+	int end_id;
+	vector<point2d<T> > point_stack;
+}segment;
 
+typedef struct st_seg_prop{
+	int seg_id;
+	int start_id;
+	int end_id;
+	int start_pix_x;
+	int start_pix_y;
+	int end_pix_x;
+	int end_pix_y;	
+}seg_property;
 
+vector HorizontalLine(point2d<int> &start, point2d<int> &end);
+vector VerticalLine(point2d<int> &start, point2d<int> &end);
+vector BresenhamLine(point2d<int> start, point2d<int>end);  
 
+class PathProc{
 
+	public:
+		
+		int map_origin_[3];
+		int map_resol_;
+		int segs_num_;
+		vector<seg_property> vec_seg_property_;
+		vector<segment<int> > vec_path_inpix_;
+		vector<segment<float> > vec_path_inmap_;
 
+		PathProc();
+		~PathProc();
+		ObatainSegProperty();
+		InterpolatingLine();
+		CatLine2Route();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
 
 #endif
--- colibri_pathproc/src/path_proc.cpp
+++ colibri_pathproc/src/path_proc.cpp
@@ -1,4 +1,157 @@
 #include "path_proc.h"
 
+void PathProc::PathProc()
+{
+
+}
+
+void PathProc::~PathProc()
+{
+
+}
+
+void PathProc::ObatainSegProperty()
+{
+
+}
+
+void PathProc::InterpolatingLine()
+{
+
+}
+
+void PathProc::CatLine2Route()
+{
+
+}
+
+
+vector HorizontalLine(point2d<int> &start, point2d<int> &end)
+{
+	vector<point2d<int> > hor_line;
+	point2d<int> tmp_point;
+
+	int index = start.x;
+	if(start.x < end.x)
+	{
+		do
+		{
+			tmp_point.x = index;
+			tmp_point.y = start.y;
+			hor_line.push_back(tmp_point);
+			index++;
+		}while(index <= end.x); 	
+	}
+	else
+	{
+		do
+		{
+			tmp_point.x = index;
+			tmp_point.y = start.y;
+			hor_line.push_back(tmp_point);
+			index--;
+		}while(index >= end.x); 	
+	}
+	return hor_line;
+
+
+}
+
+vector VerticalLine(point2d<int> &start, point2d<int> &end)
+{
+	vector<point2d<int> > ver_line;
+	point2d<int> tmp_point;
+
+	int index = start.y;
+	if(start.y < end.y)
+	{
+		do
+		{
+			tmp_point.x = start.x;
+			tmp_point.y = index;
+			ver_line.push_back(tmp_point);
+			index++;
+		}while(index <= end.y); 	
+	}
+	else
+	{
+		do
+		{
+			tmp_point.x = start.x;
+			tmp_point.y = index;
+			ver_line.push_back(tmp_point);
+			index--;
+		}while(index >= end.y); 	
+	}
+
+	return ver_line;
+
+}
+
+vector BresenhamLine(point2d<int> & start, point2d<int> & end);  
+{  
+    int dx = fabs(end.x - start.x);
+	int dy = fabs(end.y - start.y);  
+    int p = 2 * dy - dx;  
+    int twoDy = 2 * dy;
+	int twoDyMinusDx = 2 * (dy - dx);
+    int x,y;
+	vector<point2d<int> > point_at_line;
+	point2d<int> tmp_point;
+
+	if(start.x == end.x)
+	{
+	 	point_at_line = HorizontalLine(start, end);		
+	}
+	else if(start.y == end.y)
+	{
+		point_at_line = VerticalLine(start, end);
+	}
+	else	//non vertical and horizontal line
+	{
+		 bool reverse_flag = false;
+		  if(start.x > end.x)  
+		  {  
+			  x = end.x;  
+			  y = end.y;  
+			  end.x = start.x;
+			  reverse_flag = true;
+		  }  
+		  else	
+		  {  
+			  x = start.x;	
+			  y = start.y;	
+		  }  
+		  tmp_point.x = x;
+		  tmp_point.y = y;
+		  point_at_line.push_back(tmp_point);
+
+		  while(x <= end.x)	
+		  {  
+			  x++;	
+			  if(p<0)
+			  {
+				  p+=twoDy; 
+			  }
+			  else	
+			  {  
+				  y++;	
+				  p+=twoDyMinusDx;	
+			  }  
+			  tmp_point.x = x;
+			  tmp_point.y = y;
+			  point_at_line.push_back(tmp_point); 
+		  } 
+
+		  if(reverse_flag == true)
+		  {
+			  reverse(point_at_line.begin(), point_at_line.end()); 
+		  }
+
+	}
+
+	return point_at_line;	
+  
+} 
 
 
